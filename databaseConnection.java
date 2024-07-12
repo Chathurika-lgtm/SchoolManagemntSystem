7f7529d8019472076b43c0d301ccf754da82ade9
Chathurika-lgtm <chathudm0@gmail.com>
2024-07-12 09:52:08 +0530

    All file are comited

---
+++ databaseConnection.java
@@ -0,0 +1,27 @@
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class databaseConnection {
+    static Connection connection(){
+    try{
+    Class.forName("com.mysql.jdbc.Driver");
+    Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/shoolmanagement", "root", "");
+    return con;
+    }
+    catch (Exception e){
+    System.out.println(e);
+    return null;
+    }
+    }
+    
+}
